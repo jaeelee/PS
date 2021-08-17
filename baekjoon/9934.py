6ce3216647c0350592fd455ba50c28f96f0a4117
jaeelee <lje3896@gmail.com>
2021-08-17 19:03:10 +0900

    ADD: baekjoon/9934 solved

---
+++ baekjoon/9934.py
@@ -0,0 +1,30 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    9934.py                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jaeelee <jaeelee@student.42seoul.kr>       +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/08/17 18:59:28 by jaeelee           #+#    #+#              #
+#    Updated: 2021/08/17 18:59:31 by jaeelee          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def tree(k, s):
+    n = int(len(s) / 2)
+    if len(s) == 1:
+        ans[k - 1].append(s[0])
+    else: 
+        ans[k - 1].append(s[n])
+        tree(k - 1, s[:n])
+        tree(k - 1, s[n + 1:])
+
+
+
+if __name__ == '__main__':
+    k = int(input())
+    s = input().split(' ')
+    ans = [[] for i in range(k)]
+    tree(k, s)
+    for i in reversed(range(k)):
+        print(*ans[i])
