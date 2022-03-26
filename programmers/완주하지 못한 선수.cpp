3e4e946cd1bc1e777dfe39f73f5669560e99eb4e
jaeelee <lje3896@gmail.com>
2022-03-26 17:41:39 +0900

    solved

---
+++ programmers/완주하지 못한 선수.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   완주사지 못한 선수.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaeelee <jaeelee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/26 17:36:24 by jaeelee           #+#    #+#             */
+/*   Updated: 2022/03/26 17:36:24 by jaeelee          ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    
+    for (int i = 0; i < participant.size(); i++)
+    {
+        if (i >= completion.size())
+            answer = participant[i];
+        else if (participant[i].compare(completion[i]) != 0)        
+        {
+            answer = participant[i];
+            break;
+        }
+    }
+    return answer;
+}
